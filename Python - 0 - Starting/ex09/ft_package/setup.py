1e9f4ba4a5ba8d4e88b6da392c8c557830f7771a
Double7oss <elidrissielkaddiouihoussam@gmail.com>
2025-11-06 14:39:35 +0100

    Python 0 done

---
+++ Python - 0 - Starting/ex09/ft_package/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ft_package",
+    version="0.0.1",
+    author="eagle",
+    author_email="eagle@42.fr",
+    description="A sample test package",
+    url="https://github.com/eagle/ft_package",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
